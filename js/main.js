e3cfd02afc4450a62534ea3e7b9c057a8d4d5ff9
PhongLee1402 <phongtodalee1402@gmail.com>
2022-12-30 05:12:02 +0700

    Jewelry shopping

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// setInterval(function(){
+//     alert("wdqdqwdqwdqw" + Math.random())
+// },5000);
+var fullName = ("lkqwjdqkqkw");
+console.log(fullName);
+// phương pháp số học arithmetic
+console.log(2*2); 
+// phương pháp gán asignment
+var phongLe = 9;
+var lePhong = 2;
+console.log(phongLe + lePhong);
+
+// phương pháp so sánh comparition
+if(phongLe > lePhong){
+alert("sklajdlksajdklas");
+}
+if(phongLe > 0 & lePhong >0){
+    setTimeout(function(){
+        alert("9898789798");
+    },500)
+    alert("09090929090");
+}
+var numBer  = 7 ; 
+var output = ++numBer * 1 - numBer-- *4;
+console.log(output);
+
+var firstName = ("LE");
+var lastName =("PHONG");
+console.log(firstName + " " + lastName);
+
+
+console.log(firstName += " " + lastName);
+var a = 9 ; 
+var b = 6 ; 
+var isSuccess = b > a;
+
+
+console.log(isSuccess);
+
+
+
+if (undefined){
+    console.log("dieu kien dung");
+}else{
+   console.log("dieu kien sai");
+}
